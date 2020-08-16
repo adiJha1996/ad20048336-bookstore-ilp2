3724661cd960ace1d37ecb4828364ca61f38319c
adiJha1996 <47134784+adiJha1996@users.noreply.github.com>
2020-08-16 23:52:35 +0530

    update

--- src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
+++ src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.firefox.internal.ProfilesIni
+import org.openqa.selenium.firefox.internal.ProfilesIni;
 
 import org.openqa.selenium.firefox.FirefoxOptions;
 
